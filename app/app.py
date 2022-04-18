d4cab73a4218d45a51f7c65a83315bf8a084c5f8
Matic Rogar <matic.rogar@add.si>
2022-04-18 12:24:35 +0200

    adnetworks as endpoints

--- app/app.py
+++ app/app.py
@@ -9,45 +9,70 @@ Apps7 adNetwork ETL Application
 """
 
 from flask import Flask, request, jsonify
-import structlog
+import requests as r
 from datetime import datetime
+import pandas as pd
 import json
+import structlog
 
 APP = Flask(__name__)
 LOG = structlog.get_logger()
 BASE_ENDPOINT = "https://storage.googleapis.com/expertise-test/"
 
 
-@APP.route("/trigger", methods=['POST'])
-def hello_world():
+@APP.route("/SuperNetwork", methods=['POST'])
+def super_network_trigger(base_endpoint = BASE_ENDPOINT):
+  LOG.info("SuperNetwork trigger called")
   data = request.get_json()
-  json_data = jsonify(data)
-  LOG.info("Trigger called")
+  json_response_data = jsonify(data)
   dict_data = json.loads(data)
-  LOG.info(dict_data)
 
-  #date = datetime.strptime(date, "%Y-%m-%d")
-  # if adNetwork == "SuperNetwork":
-  #     date = date.strftime("%Y-%m-%d")
-  #     endpoint = base_endpoint + f"supernetwork/report/daily/{date}.csv"
-  #     req = r.get(endpoint)
-  #     req_list = [[el.strip("\r") for el in line.split(",")] for line in req.text.split("\n")]
-  #     headers = req_list.pop(0)
-  #     df_req = pd.DataFrame(req_list, columns=headers)
-  #     df_req["Revenue"] = df_req.apply(lambda row: row["Revenue"].strip("€"), axis = 1)
-  #     df_req["Currency"] = "EUR"
-  #     df_req["Date"] = pd.to_datetime(df_req["Date"])
-  #     print(df_req)
-      
-  # elif adNetwork == "AdUmbrella":
-  #     endpoint = base_endpoint + f"reporting/adumbrella/adumbrella-{date.day}_{date.month}_{date.year}.csv"
-  #     req = r.get(endpoint)
-  #     req_list = [[el.strip("\r").rstrip(" (usd)") for el in line.split(",")] for line in req.text.split("\n")]
-  #     headers = req_list.pop(0)
-  #     summary = req_list.pop(-1)
-  #     df_req = pd.DataFrame(req_list, columns=headers)
-  #     df_req["Currency"] = "USD"
-  #     df_req["Date"] = pd.to_datetime(df_req["Date"])
-  #     print(df_req)
-  #     assert "Total" in summary, "Last row was removed... But it did not contain Totals as per usual."
-  return json_data
+  LOG.info(dict_data)
+  # iterate through dates
+  for date in dict_data["date"]:
+
+    date = date.strftime("%Y-%m-%d")
+    endpoint = base_endpoint + f"supernetwork/report/daily/{date}.csv"
+    req = r.get(endpoint)
+
+    req_list = [[el.strip("\r") for el in line.split(",")] for line in req.text.split("\n")]
+    headers = req_list.pop(0)
+    df_req = pd.DataFrame(req_list, columns=headers)
+
+    df_req["Revenue"] = df_req.apply(lambda row: row["Revenue"].strip("€"), axis = 1)
+    df_req["Currency"] = "EUR"
+    df_req["Date"] = pd.to_datetime(df_req["Date"])
+
+    LOG.info(df_req)
+
+  return json_response_data
+
+
+@APP.route("/AdUmbrella", methods=['POST'])
+def super_network_trigger(base_endpoint = BASE_ENDPOINT):
+  LOG.info("SuperNetwork trigger called")
+  data = request.get_json()
+  json_response_data = jsonify(data)
+  dict_data = json.loads(data)
+
+  LOG.info(df_req)
+  # iterate through dates
+  for date in dict_data["date"]:
+
+    date = date.strftime("%Y-%m-%d")
+    endpoint = base_endpoint + f"reporting/adumbrella/adumbrella-{date.day}_{date.month}_{date.year}.csv"
+    req = r.get(endpoint)
+
+    req_list = [[el.strip("\r").rstrip(" (usd)") for el in line.split(",")] for line in req.text.split("\n")]
+    headers = req_list.pop(0)
+    summary = req_list.pop(-1)
+    df_req = pd.DataFrame(req_list, columns=headers)
+
+    df_req["Currency"] = "USD"
+    df_req["Date"] = pd.to_datetime(df_req["Date"])
+
+    LOG.info(df_req)
+    assert "Total" in summary, "Last row was removed... But it did not contain Totals as per usual."
+    
+  return json_response_data
+
